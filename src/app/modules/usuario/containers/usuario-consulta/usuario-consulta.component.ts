94f0573d506519c01c61dd15cd43ce0989b8ffee
Higorbraga01 <higorbraga00@gmail.com>
2022-11-01 16:20:14 -0300

    Inicia implementacao de usuario

--- src/app/modules/usuario/containers/usuario-consulta/usuario-consulta.component.ts
+++ src/app/modules/usuario/containers/usuario-consulta/usuario-consulta.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { UserService } from 'src/app/service/user.service';
 
 @Component({
   selector: 'app-usuario-consulta',
@@ -6,10 +7,12 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./usuario-consulta.component.scss']
 })
 export class UsuarioConsultaComponent implements OnInit {
+  data: any[];
 
-  constructor() { }
+  constructor(private userService: UserService) { }
 
   ngOnInit(): void {
+    this.userService.buscarUsuariosPorRealm().subscribe((res) => this.data = res);
   }
 
 }
--- src/app/service/user.service.ts
+++ src/app/service/user.service.ts
@@ -39,4 +39,8 @@ export class UserService {
     return of(this._user);
     //return this.http.get<any>(`${this.endpoint}/user`);
   }
+
+  buscarUsuariosPorRealm(): Observable<any[]> {
+    return this.http.get<any[]>(`${this.endpoint}user/realm/FAB`);
+  }
 }
